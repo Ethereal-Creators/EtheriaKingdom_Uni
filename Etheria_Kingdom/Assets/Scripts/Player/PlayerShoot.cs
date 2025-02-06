7923cd8d3bb1f368c46f1865df6e93c9a7e38cb0
Timingmerkat83 <pierrelucproulx@gmail.com>
2025-02-06 12:41:33 -0500

    added arrows

---
+++ Etheria_Kingdom/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField]
+    private float _bulletSpeed;
+
+    [SerializeField]
+    private Transform _gunOffset;
+
+    [SerializeField]
+    private float _timeBetweenShots; // Time between each shot (in seconds)
+
+    private void Start()
+    {
+        // Start the automatic shooting when the game starts
+        StartCoroutine(AutoShoot());
+    }
+
+    private void Update()
+    {
+        // Any additional logic you might need to check each frame
+    }
+
+    private void FireBullet()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, transform.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = _bulletSpeed * transform.up;
+    }
+
+    private IEnumerator AutoShoot()
+    {
+        // Continuously fire bullets with a time delay between each shot
+        while (true)
+        {
+            FireBullet();
+            yield return new WaitForSeconds(_timeBetweenShots); // Wait for the specified interval before shooting again
+        }
+    }
+}
+
