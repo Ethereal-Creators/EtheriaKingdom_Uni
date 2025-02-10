39177a33e509dadb979e4f9f18f4c36adfb51aed
Honmasu <112128372+Honmasu@users.noreply.github.com>
2025-02-09 22:55:25 -0500

    testroom michael changement

--- Etheria_Kingdom/Assets/Scripts/Bullet.cs
+++ Etheria_Kingdom/Assets/Scripts/Bullet.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 
 
-//public class projectile : MonoBehaviour
-//{
-//    public GameObject projectiles;
-//    public Transform pointProjectile;
-//    public float forceProjectile = 20f;
-//    public float tirInterval = 1f;
+/*public class projectile : MonoBehaviour
+{
+    public GameObject projectiles;
+    public Transform pointProjectile;
+    public float forceProjectile = 20f;
+    public float tirInterval = 1f;
     
-//    private float nextShotTime = 0f;
+    private float nextShotTime = 0f;
 
-//    void Update()
-//    {
+    void Update()
+    {
 
-//        if (Time.time >= nextShotTime)
-//        {
-//            Tire();
-//            nextShotTime = Time.time + tirInterval;
-//        }
-//    }
+        if (Time.time >= nextShotTime)
+        {
+            Tire();
+            nextShotTime = Time.time + tirInterval;
+        }
+    }
 
-//    public void Tire()
-//    {
-//        GameObject tire = Instantiate(projectiles, pointProjectile.position, pointProjectile.rotation);
-//        tire.GetComponent<Rigidbody2D>().AddForce(pointProjectile.up * forceProjectile, ForceMode2D.Impulse);
-//        Destroy(tire, 0.5f);
-//    }
-//}
+    public void Tire()
+    {
+       GameObject tire = Instantiate(projectiles, pointProjectile.position, pointProjectile.rotation);
+       tire.GetComponent<Rigidbody2D>().AddForce(pointProjectile.up * forceProjectile, ForceMode2D.Impulse);
+        Destroy(tire, 0.5f);
+    }
+}*/
 
 
 
