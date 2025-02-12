550932341c0044dedbe9a99ea4a5909af4c3b784
Jxshvfx <spiteyhd@gmail.com>
2025-02-12 14:33:07 -0500

    ~countdown final test

--- Etheria_Kingdom/Assets/Timer.cs
+++ Etheria_Kingdom/Assets/Timer.cs
@@ -9,7 +9,7 @@ public class Timer : MonoBehaviour
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
     [SerializeField] private float defaultTime = 10f;
-    [SerializeField] private Animator myAnimator;
+    public Animator myAnimator;
     public UnityEvent threeSeconds;
 
     // Update is called once per frame
@@ -22,14 +22,16 @@ public class Timer : MonoBehaviour
         else if(remainingTime < 0)
         {
             remainingTime = 0;
+            myAnimator.SetFloat("countdown", 0);
         }
 
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        
 
         if(seconds == 3) {
-            //myAnimator.setTrigger("countdown");
+            myAnimator.SetFloat("countdown", 1);
         }
     }
 
