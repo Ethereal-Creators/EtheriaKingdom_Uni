3e5a361ebed37ea62b430d77d4e63943886c7087
SparkyPX <pierrelucproulx@gmail.com>
2025-02-21 14:47:05 -0500

    Revert "yes"
    This reverts commit 34057c3ccd3ec793d34545e981edd8eb677f7318.

--- Etheria_Kingdom/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Etheria_Kingdom/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -10,20 +10,15 @@ public class EnemySpawner : MonoBehaviour
     private GameObject bigSwarmerPrefab;
 
     [SerializeField]
-    private float minSwarmerInterval = 2f;
+    private float minSwarmerInterval = 7f;
     [SerializeField]
-<<<<<<< HEAD
     private float maxSwarmerInterval = 7f;
-=======
-    private float maxSwarmerInterval = 5f; 
->>>>>>> parent of e74a5fa (oh my lerd)
     [SerializeField]
     private float minBigSwarmerInterval = 7f;
     [SerializeField]
     private float maxBigSwarmerInterval = 12f;
 
     [SerializeField]
-<<<<<<< HEAD
     private float maximumRandom = 5f;
     [SerializeField]
     private float minimumRandom = -5f;
@@ -36,12 +31,6 @@ public class EnemySpawner : MonoBehaviour
     public AudioSource source;
     public List<AudioClip> clipsStart = new List<AudioClip>();
 
-=======
-    private int maxEnemies = 10; 
-    private int currentEnemyCount = 0;
-
-    
->>>>>>> parent of e74a5fa (oh my lerd)
 
     void Start()
     {
@@ -59,7 +48,7 @@ public class EnemySpawner : MonoBehaviour
 
         if (currentEnemyCount < maxEnemies)
         {
-            GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-7f, 7f), 0), Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemy, (new Vector2(Random.Range(minimumRandom, maximumRandom), this.gameObject.transform.position.y)), Quaternion.identity);
             currentEnemyCount++;
             if (source != null && clipsStart.Count > 0)
             {
@@ -75,4 +64,28 @@ public class EnemySpawner : MonoBehaviour
     {
         currentEnemyCount--;
     }
+
+
+    /*private float timeUntilSpawn;
+
+    void awake ()
+        {
+            SetTimeUntilSpawn();
+        }
+
+        void Update()
+        {
+            minBigSwarmerInterval -= Time.deltaTime;
+
+            if(minBigSwarmerInterval <= 0)
+            {
+                Instantiate(swarmerPrefab, transform.position, Quaternion.indentity);
+                SetTimeUntilSpawn();
+            }
+        }
+
+        private void SetTimeUntilSpawn()
+        {
+            timeUntilSpawn = Random.Range(minSwarmerInterval, maxSwarmerInterval);
+        }*/
 }
