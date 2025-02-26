eb8b6dfd194ab58418095497f793585c49764814
MichaelUD <112128372+Honmasu@users.noreply.github.com>
2025-02-26 12:26:44 -0500

    laser magique

---
+++ Etheria_Kingdom/Assets/Scripts/attack_laser.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack_laser : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField]
+    private float _bulletSpeed;
+
+    [SerializeField]
+    private Transform _gunOffset;
+
+    [SerializeField]
+    private float _timeBetweenShots;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+    [SerializeField]
+    private AudioClip _shootSound;
+
+    private bool isAutoShootActive = false;
+
+    private float time = 0.0f;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= _timeBetweenShots)
+        {
+            time = 0.0f;
+            FireBullet();
+        } 
+    }
+
+    void OnEnable()
+    {
+        isAutoShootActive = false;
+        if (isAutoShootActive == false)
+        {
+            Debug.Log("auto shoot active");
+            isAutoShootActive = true;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (isAutoShootActive == true)
+        {
+            Debug.Log("auto shoot stop");
+            isAutoShootActive = false;
+        }
+    }
+
+private void FireBullet()
+{
+    Debug.Log("Shooting!");
+
+    GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, _gunOffset.rotation);
+    
+    Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+    float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+    Vector2 dir = new Vector2(Mathf.Cos(angle),Mathf.Sin(angle));
+
+
+    
+    rigidbody.velocity = _bulletSpeed * _gunOffset.up;
+
+    Destroy(bullet, 2.0f);
+
+    if (_audioSource != null && _shootSound != null)
+    {
+        _audioSource.pitch = Random.Range(0.8f, 1.2f);
+        _audioSource.PlayOneShot(_shootSound);
+    }
+}
+}
