bc5c1dd96c07159061261b95e37cbc95282d8820
Timingmerkat83 <pierrelucproulx@gmail.com>
2025-02-27 12:33:17 -0500

    sons pour grosse attaque paladin test 1 (michael)

--- Etheria_Kingdom/Assets/Scripts/paladin/heavy_attaque.cs
+++ Etheria_Kingdom/Assets/Scripts/paladin/heavy_attaque.cs
@@ -9,9 +9,14 @@ public class bigmelee : MonoBehaviour
     public float shootInterval = 1f;
     public float bulletLifetime = 5f;
 
+    [Header("------- Audio Effects Start -------")]
+    public AudioSource source;
+    public List<AudioClip> clipsStart = new List<AudioClip>();
+
     private void Awake()
     {
         _camera = Camera.main;
+        source = this.gameObject.GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -42,7 +47,17 @@ public class bigmelee : MonoBehaviour
         {
             Instantiate(bulletPrefab, transform.position, Quaternion.identity);
 
+            if (source != null && clipsStart.Count > 0)
+            {
+                int randomClipIndex = Random.Range(0, clipsStart.Count);
+                source.pitch = Random.Range(0.8f, 1.2f);
+                source.PlayOneShot(clipsStart[randomClipIndex]);
+                
+            }
+
             yield return new WaitForSeconds(shootInterval);
         }
+
+        
     }
 }
