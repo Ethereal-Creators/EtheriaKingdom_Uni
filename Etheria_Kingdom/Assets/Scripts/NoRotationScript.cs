8fcbebab9b6651e925173c095a10a0950bb2a29e
Timingmerkat83 <pierrelucproulx@gmail.com>
2025-03-12 16:56:03 -0400

    (victor) ajout destinataire power up vague 4-3

--- Etheria_Kingdom/Assets/Scripts/NoRotationScript.cs
+++ Etheria_Kingdom/Assets/Scripts/NoRotationScript.cs
@@ -6,10 +6,12 @@ public class NoRotationScript : MonoBehaviour
 {
     public float distanceTextY;
 
+    public float distanceTextXAdd = 180;
+
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.rotation = Quaternion.Euler(this.gameObject.transform.parent.rotation.x + 180, this.gameObject.transform.parent.rotation.y, 0);
+        this.gameObject.transform.rotation = Quaternion.Euler(this.gameObject.transform.parent.rotation.x + distanceTextXAdd, this.gameObject.transform.parent.rotation.y, 0);
         this.gameObject.transform.position = new Vector2(this.gameObject.transform.parent.position.x, (this.gameObject.transform.parent.position.y + distanceTextY));
     }
 }
